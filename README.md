fbe2fa56ea814e9256bd96bb607cca4630558792
nicholaspsmith <npsmith1990@gmail.com>
2025-07-30 19:42:02 -0400

    greatly simplify; remove folders & categories

--- README.md
+++ README.md
@@ -56,8 +56,6 @@
 The script will allow you to download both images and also files  
 If you don't know what I'm talking about when I say images you can locate them [here (NSFW)](https://www.pornhub.com/albums)  
 
-The script supports what I call categories, when you download a video the script will prompt you to type out a category where it will put the downloaded file there, see the image below
-
 <img src="assets/screenshot2.png" alt="Image of product 2">
 
 The script takes the full url when downloading the files, that looks like this:  
@@ -69,7 +67,6 @@ The script takes the full url when downloading the files, that looks like this:
 * [Requests](https://github.com/psf/requests)
 * [Youtube-Dl](https://github.com/ytdl-org/youtube-dl)
 * [BS4](https://www.crummy.com/software/BeautifulSoup/)
-* [pynotifier](https://github.com/YuriyLisovskiy/pynotifier)
 
 ## Getting Started
 
@@ -81,7 +78,7 @@ You need to install Python, that can be done [here](https://www.python.org)
 ### Installation
 1. Clone the repo
    ```sh
-   git clone https://github.com/logicguy1/Discord-Nitro-Generator-and-Checker.git
+   git clone git@github.com:N²/PH-DL.git
    ```
 
 2. Run the files
@@ -92,7 +89,7 @@ You need to install Python, that can be done [here](https://www.python.org)
 From there simply say yes to everything the script asks you for (after reading and agreeing of course) and you're set
 
 ## Usage
-After <a href="#installation">running the files</a> everything should be set-up and ready to go for you, if you want to download a video, press `1` and insert the URL, it will download the files, give you a notification and then you can choose where you want the video to be saved (what category)
+After <a href="#installation">running the files</a> everything should be set-up and ready to go for you.
 
 Downloading pictures and albums is a little different since you can either use a single image or a whole album
 
@@ -118,7 +115,7 @@ Contributions are what make the open source community such an amazing place to l
 
 ## Licence
 
-Copyright © 2021 Drillenissen#4268 - logicguy.mailandcontact@gmail.com
+Copyright © 2021 N² - logicguy.mailandcontact@gmail.com
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -128,6 +125,6 @@ THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR I
 
 ## Contact
 
-Logicguy - [@Drillenissen#4268](https://www.discordapp.com) - logicguy.mailandcontact@gmail.com
+Logicguy - [@N²](https://www.discordapp.com) - logicguy.mailandcontact@gmail.com
 
 Project Link: [https://github.com/logicguy1/PH-DL](https://github.com/logicguy1/PH-DL)
--- modules/videoDownloader.py
+++ modules/videoDownloader.py
@@ -1,12 +1,18 @@
-LICNECE = """
-Copyright © 2021 Drillenissen#4268 - logicguy.mailandcontact@gmail.com
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+LICENSE = """
+Copyright © 2021 N² - alphakingaustin@gmail.com
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+(the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge,
+publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR 
+ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH 
+THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
-__version__ = "1.0"
-__author__  = "Drillenissen#4268"
+__version__ = "1.1"
+__author__  = "N²"
 
 import modules.utilities as utilities
 from contextlib import contextmanager
@@ -14,57 +20,28 @@ import subprocess
 import sys, os
 import youtube_dl
 import time
-from pynotifier import Notification
 import os
 import shutil
 
 def main():
     utilities.clear()
-    url = input(" [?] Video URL: ")
+    url = input(" [?] Video URL (or 'exit' to quit): ")
+    
+    if url.lower() == "exit":
+        exit()
 
     print(" [+] Downloading stand by\n")
 
+    torrent_path = os.path.expanduser("~/Documents/Torrent")
+    video_downloads_path = os.path.join(torrent_path, "pron")
     
-    ydl = youtube_dl.YoutubeDL({'outtmpl': 'Video Downloads/%(uploader)s - %(title)s - %(id)s.%(ext)s'}) # If anyone knows how to mute the output of this send help :,)
+    ydl = youtube_dl.YoutubeDL({'outtmpl': f'{video_downloads_path}/%(uploader)s - %(title)s - %(id)s.%(ext)s'}) # If anyone knows how to mute the output of this send help :,)
 
     with ydl:
         result = ydl.extract_info(
             url,
             download = True
         )
-
-    Notification(
-    	title='Download Complete',
-    	description='Finished downloading video',
-    	duration=5,
-    	urgency='normal'
-    ).send()
-
-    print(f"\n [!] Finished downloading '{result['title']}'")
-    inp = input(" [?] Do you want to keep the video? (Y/n) ")
-
-    if "y" in inp.lower() or inp == "":
-        utilities.display_categories() # Show the avaliable categories to the user
-
-        while True:
-            category = input("\n [?] Category: ")
-
-            if not os.path.exists(f"Videos/{category}"):
-                inp = input(f" [?] Are you sure you want to create a new category named '{category}'? (Y/n) ")
-                if "y" in inp.lower() or inp == "":
-                    os.mkdir(f"Videos/{category}")
-                    break
-
-            else:
-                break
-
-        shutil.move(
-            f"Video Downloads/{result['uploader']} - {result['title']} - {result['id']}.mp4",
-            f"Videos/{category}/{result['uploader']} - {result['title']} - {result['id']}.mp4"
-        )
-
-    else:
-        os.remove(f"Video Downloads/{result['uploader']} - {result['title']} - {result['id']}.mp4")
 
 
 #https://www.pornhub.com/view_video.php?viewkey=ph5e80ec51bc6b5
